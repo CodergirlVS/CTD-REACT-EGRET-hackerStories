becd0ab9d90eefc5033bf815b456c0d27ce5023e
CodergirlVS <vaidehis1214@gmail.com>
2021-08-20 14:10:45 -0700

    lesson 1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { getByTitle } from '@testing-library/react';
+
+const title = 'React';
+
+const welcome = {
+  greetings: "Hi",
+  title:'React',
+};
+
+function getTitle(title) {
+  return title;
+};
+
+const numbers = [ 1, 2, 3, 4];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <span>{welcome.greetings} {welcome.title}</span>
+      <hr />
+      <h2>Good{getTitle(' Morning')}</h2>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <ul>
+        {numbers.map(function (number) {
+          return <li> {number * 2}</li>
+        })}
+        
+      </ul>
     </div>
   );
 }
