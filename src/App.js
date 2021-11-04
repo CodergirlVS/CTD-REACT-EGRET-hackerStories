f80afdbea5d29e29bf55c6b7b1c65183b0fc64e9
CodergirlVS <vaidehis1214@gmail.com>
2021-11-04 16:22:40 -0700

    function component converted to Class component

--- src/App.js
+++ src/App.js
@@ -18,152 +18,182 @@ function getTitle(title) {
 
 const numbers = [1, 2, 3, 4];
 
-const useSemiPersistentState = (key, initialState) => {
-  const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState
-  );
-  React.useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value, key]);
+// const useSemiPersistentState = (key, initialState) => {
+//   const [value, setValue] = React.useState(
+//     localStorage.getItem(key) || initialState
+//   );
+//   React.useEffect(() => {
+//     localStorage.setItem(key, value);
+//   }, [value, key]);
 
-  return [value, setValue];
-};
+//   return [value, setValue];
+// };
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
-const storiesReducer = (state, action) => {
-  switch (action.type) {
-    case "STORIES_FETCH_INIT":
-      return {
-        ...state,
-        isLoading: true,
-        isError: false,
-      };
-    case "STORIES_FETCH_SUCCESS":
-      return {
-        ...state,
-        data: action.payload,
-        isLoading: false,
-        isError: false,
-      };
-    case "STORIES_FETCH_FAILURE":
-      return {
-        ...state,
-        isLoading: false,
-        isError: true,
-      };
-    case "REMOVE_STORY":
-      return {
-        ...state,
-        data: state.date.filter((story) => action.payload !== story.objectID),
-      };
-    default:
-      throw new Error();
+// const storiesReducer = (state, action) => {
+//   switch (action.type) {
+//     case "STORIES_FETCH_INIT":
+//       return {
+//         ...state,
+//         isLoading: true,
+//         isError: false,
+//       };
+//     case "STORIES_FETCH_SUCCESS":
+//       return {
+//         ...state,
+//         data: action.payload,
+//         isLoading: false,
+//         isError: false,
+//       };
+//     case "STORIES_FETCH_FAILURE":
+//       return {
+//         ...state,
+//         isLoading: false,
+//         isError: true,
+//       };
+//     case "REMOVE_STORY":
+//       return {
+//         ...state,
+//         data: state.date.filter((story) => action.payload !== story.objectID),
+//       };
+//     default:
+//       throw new Error();
+//   }
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: "React",
+      isLoading: false,
+      isError: false,
+      data: [],
+      //url: `${API_ENDPOINT}${searchTerm}`,
+    };
   }
-};
 
-function App() {
-  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "Re");
-
-  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
-    data: [],
-    isLoading: false,
-    isError: false,
-  });
-  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
-
-  const handleSearchInput = (event) => {
-    setSearchTerm(event.target.value);
+  handleRemoveStory = (objectID) => {
+    return {
+      ...this.state.data,
+      this.state.date.filter((story) => action.payload !== story.objectID),
+    };
   };
 
-  const handleSearchSubmit = (event) => {
-    setUrl(`${API_ENDPOINT}${searchTerm}`);
-    event.preventDefault();
-  };
+  render() {
+    const { searchTerm } = this.state;
 
-  const handleFetchStories = React.useCallback(async () => {
-    dispatchStories({
-      type: "STORIES_FETCH_INIT",
-    });
+    return (
+      <div className="App">
+        <h1>My Hacker Stories</h1>
+        <span>
+          {welcome.greetings} {welcome.title}
+        </span>
+        <hr />
+        <h2>Good{getTitle(" Morning")}</h2>
+        <ul>
+          {numbers.map(function (number) {
+            return <li> {number * 2}</li>;
+          })}
+        </ul>
+        <hr />
 
-    try {
-      const result = await axios.get(url);
-
-      dispatchStories({
-        type: "STORIES_FETCH_SUCCESS",
-        payload: result.data.hits,
-      });
-    } catch {
-      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
-    }
-  }, [url]);
-
-  React.useEffect(() => {
-    handleFetchStories();
-  }, [handleFetchStories]);
-
-  const handleRemoveStory = (objectID) => {
-    dispatchStories({
-      type: "REMOVE_STORY",
-      payload: objectID,
-    });
-  };
-
-  const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-    <form onSubmit={onSearchSubmit}>
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        onInputChange={onSearchInput}
-        isFocused
-      >
-        <strong>Find It:</strong>
-      </InputWithLabel>
-
-      <button type="submit" disabled={!searchTerm}>
-        Submit
-      </button>
-    </form>
-  );
-
-  return (
-    <div className="App">
-      <h1>My Hacker Stories</h1>
-      <span>
-        {welcome.greetings} {welcome.title}
-      </span>
-      <hr />
-
-      <h2>Good{getTitle(" Morning")}</h2>
-
-      {/* <Search search={searchTerm} onSearch={handleSearchInput} /> */}
-
-      <SearchForm
-        searchTerm={searchTerm}
-        onSearchInput={handleSearchInput}
-        onSearchSubmit={handleSearchSubmit}
-      />
-
-      <hr />
-
-      {stories.isError && <p>Something went wrong ...</p>}
-
-      {stories.isLoading ? (
-        <p>Loading ...</p>
-      ) : (
-        <List list={stories.data} onRemoveStory={handleRemoveStory} />
-      )}
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
-      <hr />
-      <ul>
-        {numbers.map(function (number) {
-          return <li> {number * 2}</li>;
-        })}
-      </ul>
-    </div>
-  );
+        {this.state.isError && <p>Something went wrong ...</p>}
+        {this.state.isLoading ? (
+          <p>Loading ...</p>
+        ) : (
+          <List list={this.state.data} onRemoveStory={this.handleRemoveStory} />
+        )}
+        <SearchForm
+          searchTerm={searchTerm}
+          onSearchInput={(event) =>
+            this.setState({ searchTerm: event.target.value })
+          }
+          //onSearchSubmit={() => this.setState.url
+        />
+        <p>
+          Searching for <strong>{searchTerm}</strong>.
+        </p>
+        <hr />
+        
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+//   data: [],
+//   isLoading: false,
+//   isError: false,
+// });
+
+// const handleFetchStories = React.useCallback(async () => {
+//   dispatchStories({
+//     type: "STORIES_FETCH_INIT",
+//   });
+
+//   try {
+//     const result = await axios.get(url);
+
+//     dispatchStories({
+//       type: "STORIES_FETCH_SUCCESS",
+//       payload: result.data.hits,
+//     });
+//   } catch {
+//     dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+//   }
+// }, [url]);
+
+// React.useEffect(() => {
+//   handleFetchStories();
+// }, [handleFetchStories]);
+
+// const handleRemoveStory = (objectID) => {
+//   dispatchStories({
+//     type: "REMOVE_STORY",
+//     payload: objectID,
+//   });
+// };
+
+//const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+//     <form onSubmit={onSearchSubmit}>
+//       <InputWithLabel
+//         id="search"
+//         value={searchTerm}
+//         onInputChange={onSearchInput}
+//         isFocused
+//       >
+//         <strong>Find It:</strong>
+//       </InputWithLabel>
+
+//       <button type="submit" disabled={!searchTerm}>
+//         Submit
+//       </button>
+//     </form>
+//   );
+
+//   return (
+
+//       <SearchForm
+//         searchTerm={searchTerm}
+//         onSearchInput={handleSearchInput}
+//         onSearchSubmit={handleSearchSubmit}
+//       />
+
+//       <hr />
+
+//       {stories.isError && <p>Something went wrong ...</p>}
+
+//       {stories.isLoading ? (
+//         <p>Loading ...</p>
+//       ) : (
+//         <List list={stories.data} onRemoveStory={handleRemoveStory} />
+//       )}
+
+//     </div>
+//   );
+// }
--- src/InputWithLabel.jsx
+++ src/InputWithLabel.jsx
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { Component } from 'react'
 
-const InputWithLabel = ({ id, value, type = "text", onInputChange, isFocused, children }) => {
-        const inputRef = React.useRef(); 
-        React.useEffect(() => {
-        if (isFocused && inputRef.current) {
-        inputRef.current.focus();
-        }
-        });
+class InputWithLabel extends React.Component {
+constructor(props) {
+        super(props);
+
+        this.inputRef = React.createRef(); 
+}
+
+componentDidMount() {
+if (this.props.isFocused) {
+this.inputRef.current.focus();
+}
+}
+render() {
+        const {
+                id,
+                value,
+                type = 'text',
+                onInputChange,
+                isFocused,
+                children,
+        } = this.props;
 
         return (
-        <>
-                <label htmlFor={id}>{children}</label>
-                &nbsp;
-                <input
-                        id={id}
-                        type={type}
-                        ref={inputRef}
-                        value={value}
-                        onChange={onInputChange}
-                        autoFocus={isFocused}
-                />
-        </>
-        )
-};
+                <>
+                        <label htmlFor={id}>{children}</label>
+                        &nbsp;
+                        <input
+                                id={id}
+                                type={type}
+                                ref={this.inputRef}
+                                value={value}
+                                onChange={onInputChange}
+                                autoFocus={isFocused}
+                        />
+                </>
+        );
+}
+}
+
+// const InputWithLabel = ({ id, value, type = "text", onInputChange, isFocused, children }) => {
+//         const inputRef = React.useRef(); 
+//         React.useEffect(() => {
+//         if (isFocused && inputRef.current) {
+//         inputRef.current.focus();
+//         }
+//         });
 
 export default InputWithLabel;
